42c72b63f9a4e5af99cf5228fe4bb52949fabf31
Alexandre de Silva Cunha <alemac@mac.com>
2026-02-22 11:31:02 -0300

    icon fix

--- src/react/pages/index.js
+++ src/react/pages/index.js
@@ -205,8 +205,8 @@ const Clients = () => {
                   <Icon
                     name={
                       showItemsPerPageDropdown
-                        ? 'keyboard-arrow-up'
-                        : 'keyboard-arrow-down'
+                        ? 'chevron-up'
+                        : 'chevron-down'
                     }
                     size={16}
                     color="#6c757d"
